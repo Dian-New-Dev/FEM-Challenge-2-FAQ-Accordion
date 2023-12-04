2b9fe3684815ef54e9e90660cbe8455e401463c5
Diego <diannewdev@gmail.com>
2023-12-04 00:08:59 -0400

    mouse over working like a charm now, dont know why

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ console.log(questions);
 
 for (let x of questions) {
     const texToAlter = x.querySelector('h2');
-    x.addEventListener('mouseenter', function () {
-        texToAlter.classList.toggle("mouseover");
+    x.addEventListener('mouseenter', function (e) {
+        texToAlter.classList.add("mouseover");
     })
-    x.addEventListener('mouseleave', function() {
+    x.addEventListener('mouseleave', function(e) {
         console.log(e)
-        texToAlter.classList.toggle("mouseover");
+        texToAlter.classList.remove("mouseover");
 
     })
 
