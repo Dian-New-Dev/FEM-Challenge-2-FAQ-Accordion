11fce7933eac58db2155d8f118e16ba8abcc4744
Diego <diannewdev@gmail.com>
2023-12-04 19:58:45 -0400

    done, all done, dear lord it is done

--- keyboard-navigation.js
+++ keyboard-navigation.js
@@ -5,13 +5,15 @@ const questions = document.getElementsByClassName('question');
 const answers = document.querySelectorAll('p')
 const closeButtons = document.getElementsByClassName('plusSignImg')
 
+let ans = -1;
+
 document.addEventListener('keydown', function (e) {
     if (e.keyCode > 36 && e.keyCode < 41) {
-        navigateWithArrows(e);
+        activateKeyNav(e);
     }
 })
 
-function navigateWithArrows (keyPressed) {
+function activateKeyNav (keyPressed) {
     let h2Elements = [];
     for (let i = 0; i < questions.length; i++) {
         h2Elements.push(questions[i].querySelector('h2'));
@@ -21,26 +23,76 @@ function navigateWithArrows (keyPressed) {
     
     if (selectedQuestion.length === 0) {
         highlightQuestion(h2Elements[0])
+        
     } else if  (selectedQuestion.length !== 0) {
         const selected = document.getElementById('mouseover')
-        for (let i = 0; i < h2Elements.length; i++) {
-            if (selected === h2Elements[i]) {
-                questionBackToNormal(h2Elements[i])
-                highlightQuestion(h2Elements[i+1])
-                break
+            if (keyPressed.keyCode === 38) {
+                navigateup(selected, h2Elements);
             }
+            else if (keyPressed.keyCode === 40) {
+                navigatedown(selected, h2Elements)
+            }
+        
         }
-        }
-        
-        
-        
-        
-
     }
-    
-
 
 
 function hasMouseOver (value, index, array) {
     return value.id === 'mouseover';
 }
+
+function navigatedown(selected, h2Elements) {
+    for (let i = 0; i < h2Elements.length; i++) {
+        if (selected === h2Elements[i] && h2Elements[i+1] !== undefined) {
+            questionBackToNormal(h2Elements[i])
+            highlightQuestion(h2Elements[i+1])
+            
+            break
+        } else if (h2Elements[i+1] === undefined) {
+            
+        }
+    }
+    
+}
+
+function navigateup(selected, h2Elements) {
+    for (let i = 0; i < h2Elements.length; i++) {
+        if (selected === h2Elements[i] && h2Elements[i-1] !== undefined) {
+            questionBackToNormal(h2Elements[i])
+            highlightQuestion(h2Elements[i-1])
+            
+            break;
+        } else if (h2Elements[i+1] === undefined) {
+            
+        } 
+    }
+
+}
+
+// opening  questions with enter
+
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 40 && ans < 3) {
+        ans = ans + 1;
+    } else if (e.keyCode === 38 && ans > 0) {
+        ans = ans - 1;
+    } else {
+
+    }
+})
+
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        const style = window.getComputedStyle(answers[ans]);
+        if (style.display === 'none') {
+            showAnswer(answers[ans])
+        } else if (style.display === 'block') {
+            hideAnswer(answers[ans])
+        }
+                
+                    
+    }
+})
+
+// these two last functions could've done the whole thing...
+
